5585229ec98f40ceee47657d0daa4b8dfe4cae98
mifl <mifl.pc@gmail.com>
2015-01-21 16:46:31 +0100

    oscar: set ramdisk location with mkbootimg argument
    Change-Id: I4b3b17aceb33e9ce62f290f3d14fc483b3d8eed1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,6 +86,7 @@ BOARD_KERNEL_BASE               := 0x80200000
 BOARD_KERNEL_CMDLINE            := console=ttyHSL0,115200,n8 androidboot.hardware=qcom androidboot.carrier=ATT user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2 loglevel=0
 BOARD_KERNEL_PAGESIZE           := 2048
 BOARD_RAMDISK_OFFSET            := 0x02000000
+BOARD_MKBOOTIMG_ARGS            := --ramdisk_offset 0x02000000
 TARGET_KERNEL_CONFIG            := oscar_defconfig
 #TARGET_KERNEL_CUSTOM_TOOLCHAIN  := arm-eabi-4.4.3
 TARGET_KERNEL_SOURCE            := kernel/pantech/oscar
